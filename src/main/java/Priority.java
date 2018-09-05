b7b7144a20a4cdee2e9c895240432d807eef2998
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-05 13:03:50 -0300

    Adding Priority model and its unit tests

---
+++ src/main/java/Priority.java
@@ -0,0 +1,22 @@
+public enum Priority {
+   HIGH,
+   MEDIUM,
+   LOW,
+   LOWEST;
+
+   public static Priority of(Character priorityCode) {
+      Priority priority;
+      switch (priorityCode) {
+         case 'H':
+            priority = Priority.HIGH; break;
+         case 'M':
+            priority = Priority.MEDIUM; break;
+         case 'L':
+            priority = Priority.LOW; break;
+         default:
+            priority = Priority.LOWEST;
+      }
+      return priority;
+   }
+
+}
---
+++ src/test/java/PriorityTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PriorityTest {
+
+   @Test
+   @DisplayName("let: code 'H' lookup to be Priority.HIGH")
+   public void priorityCodeHShouldBeHigh() {
+      Priority p = Priority.of('H');
+      assertThat(p).isEqualTo(Priority.HIGH);
+   }
+
+   @Test
+   @DisplayName("let: code 'M' lookup to be Priority.MEDIUM")
+   public void priorityCodeMShouldBeMedium() {
+      Priority p = Priority.of('M');
+      assertThat(p).isEqualTo(Priority.MEDIUM);
+   }
+
+   @Test
+   @DisplayName("let: code 'L' lookup to be Priority.LOW")
+   public void priorityCodeLShouldBeLow() {
+      Priority p = Priority.of('L');
+      assertThat(p).isEqualTo(Priority.LOW);
+   }
+
+   @Test
+   @DisplayName("let: code 'C' lookup be Priority.LOWEST")
+   public void priorityCodeCShouldBeLowest() {
+      Priority p = Priority.of('C');
+      assertThat(p).isEqualTo(Priority.LOWEST);
+   }
+
+   @Test
+   @DisplayName("let: any other code lookup be Priority.LOWEST")
+   public void anyOtherCodeShouldBeLowest() {
+      Priority p = Priority.of('Z');
+      assertThat(p).isEqualTo(Priority.LOWEST);
+   }
+
+}
