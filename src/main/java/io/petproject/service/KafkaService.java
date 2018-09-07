d204ea1d9f4536dec5c18e21c6fb3f58ae9ea08d
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-07 20:35:32 -0300

    Implementing subscribe to listen to topics on Kafka

--- src/main/java/io/petproject/service/KafkaService.java
+++ src/main/java/io/petproject/service/KafkaService.java
@@ -2,17 +2,24 @@ package io.petproject.service;
 
 import io.petproject.model.Order;
 import org.apache.flink.api.common.ExecutionConfig;
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.serialization.TypeInformationSerializationSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 
 import java.util.Collection;
+import java.util.Properties;
 
 public class KafkaService<T> {
 
    private final static String KAFKA_SERVER = "localhost:9092";
+   private final static String ZOOKEEPER_SERVER = "localhost:2181";
+   private final static String CONSUMER_GROUP_ID = "test-flink-consumer";
+
    private StreamExecutionEnvironment sEnv;
    private Class<T> tClass;
 
@@ -28,12 +35,32 @@ public class KafkaService<T> {
       sEnv.execute();
    }
 
+   public DataStream<T> subscribe(String kafkaTopic) {
+      DataStream<T> dataStream = sEnv.addSource(getKafkaConsumer(kafkaTopic));
+      return dataStream;
+   }
+
    private FlinkKafkaProducer011<T> getKafkaProducer(String kafkaTopic) {
       TypeInformationSerializationSchema<T> serializationSchema = new TypeInformationSerializationSchema<>(
          TypeInformation.of(tClass),
          new ExecutionConfig()
       );
       return new FlinkKafkaProducer011<>(KAFKA_SERVER, kafkaTopic, serializationSchema);
+   }
+
+   public FlinkKafkaConsumer011<T> getKafkaConsumer(String kafkaTopic) {
+      DeserializationSchema<T> tDeserializationSchema = (DeserializationSchema<T>) new SimpleStringSchema();
+      FlinkKafkaConsumer011<T> kafkaConsumer = new FlinkKafkaConsumer011<>(kafkaTopic, tDeserializationSchema, getKafkaConsumerProperties());
+      kafkaConsumer.setStartFromEarliest(); // for testing purposes-only
+      return kafkaConsumer;
+   }
+
+   private Properties getKafkaConsumerProperties() {
+      Properties kafkaConsumer = new Properties();
+      kafkaConsumer.setProperty("bootstrap.servers", KAFKA_SERVER);
+      kafkaConsumer.setProperty("zookeeper.connect", ZOOKEEPER_SERVER);
+      kafkaConsumer.setProperty("group.id", CONSUMER_GROUP_ID);
+      return kafkaConsumer;
    }
 
 }
