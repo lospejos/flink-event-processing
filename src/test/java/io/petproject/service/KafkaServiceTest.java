d50a2acca6d56f4b5f1950c247eb1a550cdc8b95
Bruno Oliveira <bruno.ariev@gmail.com>
2018-09-07 21:29:26 -0300

    Updating test to also consume messages from the given topic after publishing

--- src/test/java/io/petproject/service/KafkaServiceTest.java
+++ src/test/java/io/petproject/service/KafkaServiceTest.java
@@ -3,15 +3,25 @@ package io.petproject.service;
 import io.petproject.model.Order;
 import io.petproject.model.Priority;
 import io.petproject.model.SalesOrder;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class KafkaServiceTest {
 
-   private final static String KAKFA_TOPIC = "kafka-test-topic";
+   private final static String KAFKA_TEST_TOPIC = "kafka-test-topic";
    private List<Order> orders;
 
    @BeforeEach
@@ -33,9 +43,35 @@ public class KafkaServiceTest {
    }
 
    @Test
-   public void shouldPublishOrdersToKafka() throws Exception {
+   @DisplayName("let: publish/subscribe send and receive messages to/from Kafka")
+   public void shouldPublishToAndConsumeFromKafka() throws Exception {
       KafkaService<Order> kafkaService = new KafkaService<>();
-      kafkaService.publish(KAKFA_TOPIC, orders);
+      kafkaService.publish(KAFKA_TEST_TOPIC, orders);
+
+      DataStream<Order> consumerStream = kafkaService.subscribe(KAFKA_TEST_TOPIC);
+      consumerStream.print();
+      ByteArrayOutputStream sysOutReturn = setupSysOutToCapturePrint(kafkaService);
+
+      assertThat(sysOutReturn.toString().split("\n").length)
+         .isGreaterThanOrEqualTo(orders.size());
+   }
+
+   private ByteArrayOutputStream setupSysOutToCapturePrint(KafkaService<?> kafkaService) throws InterruptedException {
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      System.setOut(new PrintStream(outputStream));
+      System.setErr(new PrintStream(new ByteArrayOutputStream()));
+
+      CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+         try {
+            kafkaService.getStreamEnv().execute();
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+      });
+
+      future.orTimeout(5, TimeUnit.SECONDS);
+      Thread.sleep(5000);
+      return outputStream;
    }
 
 }
