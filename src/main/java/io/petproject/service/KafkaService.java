033caccbbf1cfb66cf7ac594cd87e39e455fa64c
Bruno Oliveira <bruno.ariev@gmail.com>
2018-10-27 13:08:00 -0300

    Adding ProducerProperties, extra ConsumerProperties, and other minor refactorings

--- src/main/java/io/petproject/service/KafkaService.java
+++ src/main/java/io/petproject/service/KafkaService.java
@@ -7,8 +7,13 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.flink.streaming.util.serialization.KeyedDeserializationSchemaWrapper;
+import org.apache.flink.streaming.util.serialization.KeyedSerializationSchemaWrapper;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Properties;
 
 public class KafkaService<T> {
@@ -35,9 +40,10 @@ public class KafkaService<T> {
 
    private FlinkKafkaProducer011<T> getKafkaProducer(String kafkaTopic) {
       var kafkaProducer = new FlinkKafkaProducer011<>(
-         kafkaConfig.getProperty("kafka.producer.bootstrap-server"),
          kafkaTopic,
-         getSerializationSchema()
+         new KeyedSerializationSchemaWrapper<>(getSerializationSchema()),
+         getProducerProperties(),
+         FlinkKafkaProducer011.Semantic.AT_LEAST_ONCE
       );
       kafkaProducer.setWriteTimestampToKafka(true);
       return kafkaProducer;
@@ -46,8 +52,8 @@ public class KafkaService<T> {
    private FlinkKafkaConsumer011<T> getKafkaConsumer(String kafkaTopic) {
       var kafkaConsumer = new FlinkKafkaConsumer011<>(
          kafkaTopic,
-         getSerializationSchema(),
-         getKafkaConsumerProperties()
+         new KeyedDeserializationSchemaWrapper<>(getSerializationSchema()),
+         getConsumerProperties()
       );
       kafkaConsumer.setStartFromGroupOffsets();
       return kafkaConsumer;
@@ -57,16 +63,22 @@ public class KafkaService<T> {
       return new TypeInformationSerializationSchema<>(TypeInformation.of(tClass), new ExecutionConfig());
    }
 
-   private Properties getKafkaConsumerProperties() {
-      Properties kafkaConsumer = new Properties();
-      kafkaConsumer.setProperty("bootstrap.servers", kafkaConfig.getProperty("kafka.consumer.bootstrap-server"));
-      kafkaConsumer.setProperty("zookeeper.connect", kafkaConfig.getProperty("kafka.consumer.zookeeper-server"));
-      kafkaConsumer.setProperty("group.id", kafkaConfig.getProperty("kafka.consumer.group-id"));
-      return kafkaConsumer;
+   private Properties getProducerProperties() {
+      Properties kafkaProducerProps = new Properties();
+      kafkaProducerProps.putAll(Map.of(
+         ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getProperty("kafka.producer.bootstrap-servers")
+      ));
+      return kafkaProducerProps;
    }
 
-   protected StreamExecutionEnvironment getStreamEnv() {
-      return streamEnv;
+   private Properties getConsumerProperties() {
+      Properties kafkaConsumerProps = new Properties();
+      kafkaConsumerProps.putAll(Map.of(
+         ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getProperty("kafka.consumer.bootstrap-servers"),
+         ConsumerConfig.GROUP_ID_CONFIG, kafkaConfig.getProperty("kafka.consumer.group-id"),
+         ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true
+      ));
+      return kafkaConsumerProps;
    }
 
 }
